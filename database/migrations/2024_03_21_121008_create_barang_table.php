5e2fa582fd08df60def9b5a15e66a8df8a72d5e5
mrbudbud <rachmad.16421044@student.ubl.ac.id>
2024-03-23 07:24:53 +0700

    migration barang

--- database/migrations/2024_03_21_121008_create_barang_table.php
+++ database/migrations/2024_03_21_121008_create_barang_table.php
@@ -20,8 +20,10 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        DB::table('barang')->insert(['img' => 'user/img/eigerShouse.jpg', 'nama' => 'sepatu eiger', 'harga' => 1200000]);
-        DB::table('barang')->insert(['img' => 'user/img/everestTitaniumConsina.jpg ', 'nama' => 'Everest 50L Consina', 'harga' => 2000000]);
+        DB::table('barang')->insert(['id' => 2,  'img' => 'user/img/everestTitaniumConsina.jpg', 'nama' => 'Everest 50L Consina', 'harga' => 2000000]);
+        DB::table('barang')->insert(['id' => 4,  'img' => 'user/barang/2u0y4evbjTemDQR6zziJSeL30sdpucLvonyYBi2o.jpg', 'nama' => 'Sleping Pad Consina', 'harga' => 170000]);
+        DB::table('barang')->insert(['id' => 5,  'img' => 'user/barang/6m3MpLf3Ij3V6BDJymZyqbYd7JGdFFXZST9I2pZ6.jpg', 'nama' => 'Consina TPJ 43 DGR', 'harga' => 80000]);
+        DB::table('barang')->insert(['id' => 6,  'img' => 'user/barang/6wyzQUK54dQ0HYprrES8Nzwrqc3lJukDP9mRKi5p.jpg', 'nama' => 'Mamba Mid Eiger', 'harga' => 1200000]);
     }
 
     /**
